ba3b96fce2105ff3ade8c8d49dc14a23c6b2b997
Tanish Mahajan <tanish.mahajan@nagarro.com>
2020-04-16 20:04:38 +0530

    make it better

--- public/edit.js
+++ public/edit.js
@@ -1,4 +1,7 @@
 async function Edit(id){
+  var date = document.getElementById('due')
+var tomorrowLocal = new Date(new Date().getTime() + 24 * 60 * 60 * 1000 - new Date().getTimezoneOffset() * 60 * 1000).toISOString().substr(0,10);
+date.value = tomorrowLocal
     document.getElementById('id02').style.display='block'
     id = Number(id)
     const update = document.getElementById("update")
