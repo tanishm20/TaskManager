133053491a29b8656e7e9e2cda7b1ad86852c684
Tanish Mahajan <tanish.mahajan@nagarro.com>
2020-04-16 17:22:19 +0530

    make it better

--- public/script.js
+++ public/script.js
@@ -1,3 +1,6 @@
+var date = document.getElementById('due')
+var tomorrowLocal = new Date(new Date().getTime() + 24 * 60 * 60 * 1000 - new Date().getTimezoneOffset() * 60 * 1000).toISOString().substr(0,10);
+date.value = tomorrowLocal
 window.onload= async function getTodos() {
   const resp = await fetch('/todos', { method: 'GET' })
   const todos = await resp.json()
